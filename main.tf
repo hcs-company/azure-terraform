8616d6c0f50612c2c523453c5e3f20b244933513
Marcel Kerker <mkerker@mbp-van-marcel.home>
2019-10-18 15:12:56 +0200

    create Azure VM node

--- main.tf
+++ main.tf
@@ -1,63 +1,63 @@
 variable "admin_username" {
-  default = "k3suser"
+  default = "adminuser"
 }
-variable "admin_password" {
-  default = "Password1234!"
-}
+#variable "admin_password" {
+#  default = "Password1234!"
+#}
 
 # Configure the provider
 provider "azurerm" {
 }
 
 # Create a new resource group
-resource "azurerm_resource_group" "k3s" {
+resource "azurerm_resource_group" "node" {
   name     = "azure_rg"
   location = "westeurope"
 
 }
 
-resource "azurerm_virtual_network" "k3s" {
+resource "azurerm_virtual_network" "node" {
   name                = "azure_vn"
   address_space       = ["10.0.0.0/16"]
-  location            = "${azurerm_resource_group.k3s.location}"
-  resource_group_name = "${azurerm_resource_group.k3s.name}"
+  location            = "${azurerm_resource_group.node.location}"
+  resource_group_name = "${azurerm_resource_group.node.name}"
 }
 
-resource "azurerm_subnet" "k3s" {
+resource "azurerm_subnet" "node" {
   name                 = "azure_sub"
-  resource_group_name  = "${azurerm_resource_group.k3s.name}"
-  virtual_network_name = "${azurerm_virtual_network.k3s.name}"
+  resource_group_name  = "${azurerm_resource_group.node.name}"
+  virtual_network_name = "${azurerm_virtual_network.node.name}"
   address_prefix       = "10.0.2.0/24"
 }
 
-resource "azurerm_public_ip" "k3s" {
+resource "azurerm_public_ip" "node" {
   name                    = "azure_pip"
-  location                = "${azurerm_resource_group.k3s.location}"
-  resource_group_name     = "${azurerm_resource_group.k3s.name}"
+  location                = "${azurerm_resource_group.node.location}"
+  resource_group_name     = "${azurerm_resource_group.node.name}"
   allocation_method       = "Dynamic"
   idle_timeout_in_minutes = 30
-  domain_name_label       = "k3s-rancher"
+  domain_name_label       = "node-rancher"
 }
 
-resource "azurerm_network_interface" "k3s" {
+resource "azurerm_network_interface" "node" {
   name                = "azure_ni"
-  location            = "${azurerm_resource_group.k3s.location}"
-  resource_group_name = "${azurerm_resource_group.k3s.name}"
+  location            = "${azurerm_resource_group.node.location}"
+  resource_group_name = "${azurerm_resource_group.node.name}"
   ip_configuration {
     name                          = "azure_ip"
-    subnet_id                     = "${azurerm_subnet.k3s.id}"
+    subnet_id                     = "${azurerm_subnet.node.id}"
     private_ip_address_allocation = "static"
     private_ip_address            = "10.0.2.5"
-    public_ip_address_id          = "${azurerm_public_ip.k3s.id}"
+    public_ip_address_id          = "${azurerm_public_ip.node.id}"
   }
 }
 
 # Create a Linux virtual machine
-resource "azurerm_virtual_machine" "k3s" {
+resource "azurerm_virtual_machine" "node" {
   name                  = "azure_vm"
   location              = "westeurope"
-  resource_group_name   = "${azurerm_resource_group.k3s.name}"
-  network_interface_ids = ["${azurerm_network_interface.k3s.id}"]
+  resource_group_name   = "${azurerm_resource_group.node.name}"
+  network_interface_ids = ["${azurerm_network_interface.node.id}"]
   vm_size               = "Standard_B1S"
 
   storage_os_disk {
@@ -75,7 +75,7 @@ resource "azurerm_virtual_machine" "k3s" {
   }
 
   os_profile {
-    computer_name  = "azurek3snode"
+    computer_name  = "azurenode"
     admin_username = "${var.admin_username}"
     admin_password = "${var.admin_password}"
   }
@@ -86,13 +86,14 @@ resource "azurerm_virtual_machine" "k3s" {
 
   provisioner "remote-exec" {
     connection {
-      host     = "${azurerm_public_ip.k3s.fqdn}"
+      host     = "${azurerm_public_ip.node.fqdn}"
       type     = "ssh"
       user     = "${var.admin_username}"
       password = "${var.admin_password}"
     }
     inline = [
-      "sudo curl -sfL https://get.k3s.io | sh -"
+      #"sudo curl -sfL https://get.k3s.io | sh -"
+      "ls -al"
     ]
   }
 }
