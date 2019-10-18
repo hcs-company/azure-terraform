e7c131b817c07a6ad2434ddeecd39e93359c9834
Marcel Kerker <mkerker@mbp-van-marcel.home>
2019-10-18 14:16:37 +0200

    k3s

--- main.tf
+++ main.tf
@@ -1,155 +1,98 @@
-# Configure the Microsoft Azure Provider
+variable "admin_username" {
+  default = "k3suser"
+}
+variable "admin_password" {
+  default = "Password1234!"
+}
+
+# Configure the provider
 provider "azurerm" {
 }
 
-# Create a resource group if it doesn’t exist
-resource "azurerm_resource_group" "myterraformgroup" {
-    name     = "myResourceGroup"
-    location = "eastus"
+# Create a new resource group
+resource "azurerm_resource_group" "k3s" {
+  name     = "azure_rg"
+  location = "westeurope"
 
-    tags = {
-        environment = "Terraform Demo"
-    }
 }
 
-# Create virtual network
-resource "azurerm_virtual_network" "myterraformnetwork" {
-    name                = "myVnet"
-    address_space       = ["10.0.0.0/16"]
-    location            = "eastus"
-    resource_group_name = "${azurerm_resource_group.myterraformgroup.name}"
-
-    tags = {
-        environment = "Terraform Demo"
-    }
+resource "azurerm_virtual_network" "k3s" {
+  name                = "azure_vn"
+  address_space       = ["10.0.0.0/16"]
+  location            = "${azurerm_resource_group.k3s.location}"
+  resource_group_name = "${azurerm_resource_group.k3s.name}"
 }
 
-# Create subnet
-resource "azurerm_subnet" "myterraformsubnet" {
-    name                 = "mySubnet"
-    resource_group_name  = "${azurerm_resource_group.myterraformgroup.name}"
-    virtual_network_name = "${azurerm_virtual_network.myterraformnetwork.name}"
-    address_prefix       = "10.0.1.0/24"
+resource "azurerm_subnet" "k3s" {
+  name                 = "azure_sub"
+  resource_group_name  = "${azurerm_resource_group.k3s.name}"
+  virtual_network_name = "${azurerm_virtual_network.k3s.name}"
+  address_prefix       = "10.0.2.0/24"
 }
 
-# Create public IPs
-resource "azurerm_public_ip" "myterraformpublicip" {
-    name                         = "myPublicIP"
-    location                     = "eastus"
-    resource_group_name          = "${azurerm_resource_group.myterraformgroup.name}"
-    allocation_method            = "Dynamic"
-
-    tags = {
-        environment = "Terraform Demo"
-    }
+resource "azurerm_public_ip" "k3s" {
+  name                    = "azure_pip"
+  location                = "${azurerm_resource_group.k3s.location}"
+  resource_group_name     = "${azurerm_resource_group.k3s.name}"
+  allocation_method       = "Dynamic"
+  idle_timeout_in_minutes = 30
+  domain_name_label       = "k3s-rancher"
 }
 
-# Create Network Security Group and rule
-resource "azurerm_network_security_group" "myterraformnsg" {
-    name                = "myNetworkSecurityGroup"
-    location            = "eastus"
-    resource_group_name = "${azurerm_resource_group.myterraformgroup.name}"
-
-    security_rule {
-        name                       = "SSH"
-        priority                   = 1001
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "22"
-        source_address_prefix      = "*"
-        destination_address_prefix = "*"
-    }
-
-    tags = {
-        environment = "Terraform Demo"
-    }
+resource "azurerm_network_interface" "k3s" {
+  name                = "azure_ni"
+  location            = "${azurerm_resource_group.k3s.location}"
+  resource_group_name = "${azurerm_resource_group.k3s.name}"
+  ip_configuration {
+    name                          = "azure_ip"
+    subnet_id                     = "${azurerm_subnet.k3s.id}"
+    private_ip_address_allocation = "static"
+    private_ip_address            = "10.0.2.5"
+    public_ip_address_id          = "${azurerm_public_ip.k3s.id}"
+  }
 }
 
-# Create network interface
-resource "azurerm_network_interface" "myterraformnic" {
-    name                      = "myNIC"
-    location                  = "eastus"
-    resource_group_name       = "${azurerm_resource_group.myterraformgroup.name}"
-    network_security_group_id = "${azurerm_network_security_group.myterraformnsg.id}"
+# Create a Linux virtual machine
+resource "azurerm_virtual_machine" "k3s" {
+  name                  = "azure_vm"
+  location              = "westeurope"
+  resource_group_name   = "${azurerm_resource_group.k3s.name}"
+  network_interface_ids = ["${azurerm_network_interface.k3s.id}"]
+  vm_size               = "Standard_B1S"
 
-    ip_configuration {
-        name                          = "myNicConfiguration"
-        subnet_id                     = "${azurerm_subnet.myterraformsubnet.id}"
-        private_ip_address_allocation = "Dynamic"
-        public_ip_address_id          = "${azurerm_public_ip.myterraformpublicip.id}"
+  storage_os_disk {
+    name              = "azure_od"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Premium_LRS"
+  }
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  os_profile {
+    computer_name  = "azurek3snode"
+    admin_username = "${var.admin_username}"
+    admin_password = "${var.admin_password}"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      host     = "${azurerm_public_ip.k3s.fqdn}"
+      type     = "ssh"
+      user     = "${var.admin_username}"
+      password = "${var.admin_password}"
     }
-
-    tags = {
-        environment = "Terraform Demo"
-    }
-}
-
-# Generate random text for a unique storage account name
-resource "random_id" "randomId" {
-    keepers = {
-        # Generate a new ID only when a new resource group is defined
-        resource_group = "${azurerm_resource_group.myterraformgroup.name}"
-    }
-
-    byte_length = 8
-}
-
-# Create storage account for boot diagnostics
-resource "azurerm_storage_account" "mystorageaccount" {
-    name                        = "diag${random_id.randomId.hex}"
-    resource_group_name         = "${azurerm_resource_group.myterraformgroup.name}"
-    location                    = "eastus"
-    account_tier                = "Standard"
-    account_replication_type    = "LRS"
-
-    tags = {
-        environment = "Terraform Demo"
-    }
-}
-
-# Create virtual machine
-resource "azurerm_virtual_machine" "myterraformvm" {
-    name                  = "myVM"
-    location              = "eastus"
-    resource_group_name   = "${azurerm_resource_group.myterraformgroup.name}"
-    network_interface_ids = ["${azurerm_network_interface.myterraformnic.id}"]
-    vm_size               = "Standard_DS1_v2"
-
-    storage_os_disk {
-        name              = "myOsDisk"
-        caching           = "ReadWrite"
-        create_option     = "FromImage"
-        managed_disk_type = "Premium_LRS"
-    }
-
-    storage_image_reference {
-        publisher = "Canonical"
-        offer     = "UbuntuServer"
-        sku       = "16.04.0-LTS"
-        version   = "latest"
-    }
-
-    os_profile {
-        computer_name  = "myvm"
-        admin_username = "azureuser"
-    }
-
-    os_profile_linux_config {
-        disable_password_authentication = true
-        ssh_keys {
-            path     = "/home/azureuser/.ssh/authorized_keys"
-            key_data = "ssh-rsa AAAAB3Nz{snip}hwhqT9h"
-        }
-    }
-
-    boot_diagnostics {
-        enabled = "true"
-        storage_uri = "${azurerm_storage_account.mystorageaccount.primary_blob_endpoint}"
-    }
-
-    tags = {
-        environment = "Terraform Demo"
-    }
+    inline = [
+      "sudo curl -sfL https://get.k3s.io | sh -"
+    ]
+  }
 }
